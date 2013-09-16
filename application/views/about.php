d31fcf44a2866fe84d4d59d41331a750fb68160c
Arbaaz <arbaaz@triacsolutions.com>
2013-09-16 13:47:55 +0530

    This is the root ci packages added

---
+++ application/views/about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+<title>about us</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="<?=asset_url()?>css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="<?=asset_url()?>css/style.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+<div class="jumbotron">
+	<p>hello this is a bootstrap</p>
+</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?=asset_url()?>js/bootstrap.min.js"></script>
+  </body>
+</html>
